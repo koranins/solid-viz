a43f7b551402feea2951e73022eede1119cd2f0b
Kor <koranin.suwannaprasert@alumni.scu.edu>
2023-11-29 21:12:43 -0600

    create bun project

---
+++ src/routes/fibonacci.tsx
@@ -0,0 +1,13 @@
+import { Title } from 'solid-start'
+
+const Coin = () => (
+    <main>
+        <Title>Fibonacci</Title>
+        <h1>Fibonacci!</h1>
+        <p>
+            More stuff here
+        </p>
+    </main>
+)
+
+export default Coin
